991f5f71de052ba56645ac38d0c1e49955861d07
Juho Syrjänen <juho.syrjanen@fraktio.fi>
2021-07-20 15:18:51 +0300

    Kippis watcher

--- kippis_watcher.py
+++ kippis_watcher.py
@@ -19,10 +19,10 @@ def check_mentions(api, keywords, since_id):
         if tweet.in_reply_to_status_id is not None:
             continue
         if any(keyword in tweet.text.lower() for keyword in keywords):
-            logger.info(f"Answering to {tweet.user.name}")
+            logger.info(f"Answering to {tweet.user.name} - Twitter handle is {tweet.user.screen_name}")
 
             api.update_status(
-                status="Kippis!",
+                status="@" +tweet.user.screen_name+ " Kippis!",
                 in_reply_to_status_id=tweet.id,
             )
     return new_since_id
