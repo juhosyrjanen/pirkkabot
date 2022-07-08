73ede68108efd91c63d35b3f8c47713ea1b17bac
Juho Syrjänen <juho@syrjanen.org>
2022-07-08 15:29:22 +0300

    fix: Price parsing error fix (#12)

--- tweet.py
+++ tweet.py
@@ -8,22 +8,23 @@ import sqlite3
 from sqlite3 import Error
 
 #get today's pirkka price
-with open('price_today', 'r') as file:
-    price = file.read().replace('\n', '')
+price_raw = open('price_today', 'r')
+price = price_raw.readline()
+price_float = float(price)
 
 #date formating
 date_today = date.today()
 date_formated = date_today.strftime(' %d.%m.%Y ')
 
 #twitter API auth
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth)
+#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#auth.set_access_token(access_token, access_secret)
+#api = tweepy.API(auth)
 
 #compose tweet and shoot
 tweet = "Pirkka III-oluen hinta tänään"+ date_formated +"on " + price + "€."
 print(tweet)
-api.update_status(status=tweet)
+#api.update_status(status=tweet)
 
 ## Database section ##
 
@@ -40,7 +41,7 @@ try:
             ); """
 
     cursor.execute(pirkka_table)
-    cursor.execute(f"INSERT INTO PIRKKA_PRICE (PRICE, TIMESTAMP) VALUES ({price}, datetime('now'))")
+    cursor.execute(f"INSERT INTO PIRKKA_PRICE (PRICE, TIMESTAMP) VALUES ({price_float}, datetime('now'))")
     sqliteConnection.commit()
     cursor.close()
 
