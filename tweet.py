6be27fc12a86d837eae12fc47094c2eaf9152a4c
Juho Syrjänen <juho.syrjanen@polarsquad.com>
2022-12-15 16:03:35 +0200

    feat: Bring back trusty Selenium and add some error handling

--- tweet.py
+++ tweet.py
@@ -6,30 +6,60 @@ import tweepy
 from secrets import *
 import sqlite3
 from sqlite3 import Error
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-#get today's pirkka price
-with open('price_today') as price_raw:
-    price = price_raw.readline().replace('\n', '').replace(',','.')
-    price_float = float(price)
+# WebDriver options, headless, window size, user agent
+# Make sure to update user agent regurarly
+options = webdriver.ChromeOptions()
+options.headless = True
+options.add_argument("window-size=1920x1080")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument("--headless")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
 
-with open('price_today') as price_raw:
-    price_totweet = price_raw.readline().replace('\n', '')
+# Set up webdriver
+driver = webdriver.Chrome(options=options)
+driver.get('https://www.k-ruoka.fi/kauppa/tuote/pirkka-iii-olut-033l-45-tlk-si-6410405091260')   
 
-#date formating
+# Parse Pirkka III-beer price from page source 
+# Catch error if price is not found
+try:
+    price = driver.find_element(By.CLASS_NAME,'price')
+except:
+    print('Price not found')
+    driver.quit()
+    exit()
+
+# Transform price into float   
+price_float = float(price.text.replace(',','.'))
+
+# Get today's date and format it to day month year
 date_today = date.today()
-date_formated = date_today.strftime(' %d.%m.%Y ')
+date_formated = date_today.strftime('%d.%m.%Y ')
 
-#twitter API auth
+# Twitter API auth
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
-#compose tweet and shoot
-tweet = "Pirkka III-oluen hinta tänään"+ date_formated +"on " + price_totweet + "€."
+# Compose tweet and shoot
+tweet = "Pirkka III-oluen hinta tänään "+ date_formated +"on " + price.text + "€."
 print(tweet)
-api.update_status(status=tweet)
+
+# Catch error if Tweet is unsuccessful
+try:
+   print("tweet")
+   api.update_status(status=tweet)
+except:
+    print('Tweet not sent')
+    driver.quit()
+    exit()
+
+# Close the driver
+driver.quit()
 
 ## Database section ##
-
 try:
     # Connect to DB and create a cursor
     sqliteConnection = sqlite3.connect('db/pirkka_price.db')
