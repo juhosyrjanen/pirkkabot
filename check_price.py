84a8e2a6fabb1af32d7b8f645e8d6022f4457e6a
Juho Syrjänen <juho.syrjanen@polarsquad.com>
2022-12-22 10:42:51 +0200

    feat: PirkkApi

--- check_price.py
+++ check_price.py
@@ -14,7 +14,7 @@ def define_price():
     response = requests.get(
           url='https://proxy.scrapeops.io/v1/',
           params={
-              'api_key': '(api_key)',
+              'api_key': 'API_KEY',
               'url': 'https://www.k-ruoka.fi/kauppa/tuote/pirkka-iii-olut-033l-45-tlk-si-6410405091260', 
               'render_js': 'true', 
               'residential': 'true', 
---
+++ index.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler
+from check_price import define_price 
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        price = define_price()
+        self.wfile.write(price.text)
+        return
