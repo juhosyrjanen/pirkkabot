85e9cc9e037a30dad5250e77b86f2aaadee51cb6
Juho Syrjänen <juho.syrjanen@polarsquad.com>
2023-01-12 15:45:07 +0200

    fix: Add Flask Cache to limit scraping

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask_caching import Cache
 import os
 import requests
 from bs4 import BeautifulSoup
@@ -13,33 +14,32 @@ ch.setLevel(logging.DEBUG)
 # Add the stream handler to the logger
 logger.addHandler(ch) 
 
-cache = {}
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
+@cache.memoize(timeout=86400)
 def define_price():
-    if 'price' in cache:
-        return cache['price']
+    response = requests.get(
+          url='https://proxy.scrapeops.io/v1/',
+          params={
+              'api_key': os.environ.get('API_KEY'),
+              'url': 'https://www.k-ruoka.fi/kauppa/tuote/pirkka-iii-olut-033l-45-tlk-si-6410405091260', 
+              'render_js': 'true', 
+              'residential': 'true', 
+              'country': 'fi', 
+          },
+        )
+    soup = BeautifulSoup(response.content, 'html.parser')
+    price = soup.find('span', class_='price')
+    if price is None:
+        raise ValueError('Price is None')
     else:
-        response = requests.get(
-              url='https://proxy.scrapeops.io/v1/',
-              params={
-                  'api_key': os.environ.get('API_KEY'),
-                  'url': 'https://www.k-ruoka.fi/kauppa/tuote/pirkka-iii-olut-033l-45-tlk-si-6410405091260', 
-                  'render_js': 'true', 
-                  'residential': 'true', 
-                  'country': 'fi', 
-              },
-            )
-        soup = BeautifulSoup(response.content, 'html.parser')
-        price = soup.find('span', class_='price')
-        if price is None:
-            raise ValueError('Price is None')
-        else:
-            cache['price'] = price
-            return price
+        logger.info('Price fetched')
+        return price
 
 while True:
     try:
         price = define_price()
+        logger.info('Price received from define_price()')
         break
     except NameError as e:
         logger.exception("An error occurred: %s", e)
